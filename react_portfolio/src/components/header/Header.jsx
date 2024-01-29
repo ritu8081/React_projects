89deb377c4a3cddd4df8472b094fe2ba0497fd1a
ritu8081 <jadhavritu18@gmail.com>
2024-01-29 21:42:18 +0530

    header component done

--- react_portfolio/src/components/header/Header.jsx
+++ react_portfolio/src/components/header/Header.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import './Header.css'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
  const Header = () => {
   return (
 
-    <div>Header
-
-    </div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello i'm</h5>
+        <h1>Ritu Jadhav</h1>
+        <h5 className="text-light">
+          FullStack Developer
+        </h5>
+        <CTA/>
+        <HeaderSocials/>
+        <div className="me">
+          <img src={ME} alt="me" />
+          <a href="#contact" className='scroll__down'>Scroll Down</a>
+        </div>
+      </div>
+    </header>
   )
 }
 
---
+++ react_portfolio/src/components/header/HeaderSocials.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+
+
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://linkedin.com" target='_blank'><FaLinkedin /></a>
+        <a href="https://github.com" target='_blank'><FaGithub /></a>
+        <a href="https://dribble.com" target='_blank'><FaInstagram /></a>
+
+    </div>
+  )
+}
+
+
+export default HeaderSocials;
