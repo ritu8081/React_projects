3237ebd798e329aa6c7b7a4d31dd6087218d588d
ritu8081 <jadhavritu18@gmail.com>
2024-02-02 17:40:11 +0530

    colour changes

--- portfolio_react/src/utils/Themes.js
+++ portfolio_react/src/utils/Themes.js
@@ -1,15 +1,35 @@
 export const darkTheme = {
-    bg:"#1C1C27",
-    bgLight: "#1C1E27",
-    primary:"#854CE6",
+    bg:"#000000",
+    bgLight: "#1CAA1A",
+    primary:"#1CAA1A",
     text_primary:"#F2F3F4",
     text_secondary:"#b1b2b3",
-    card:"#171721",
-    card_light: '#191924',
-    button:"#854CE6",
+    card:"#000000",
+    card_light: '#000000',
+    button:'#1CAA1A',
     white:"#FFFFFF",
     black:"#000000",
 }
+
+
+
+
+
+
+
+
+// export const darkTheme = {
+//     bg:"#1C1C27",
+//     bgLight: "#1C1E27",
+//     primary:"#854CE6",
+//     text_primary:"#F2F3F4",
+//     text_secondary:"#b1b2b3",
+//     card:"#171721",
+//     card_light: '#191924',
+//     button:"#854CE6",
+//     white:"#FFFFFF",
+//     black:"#000000",
+// }
 
 export const lightTheme = {
     bg:"#FFFFFF",
